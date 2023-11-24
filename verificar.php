cf6003fd02db667d110b0821d4f2136329992928
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-24 17:00:06 -0300

    ok

--- verificar.php
+++ verificar.php
@@ -2,12 +2,17 @@
     $chaveEvento = trim($_POST['chave_evento']);
     $chavePessoal = trim($_POST['chave_pessoal']);    
     $myfile = fopen($chaveEvento.".txt", "r") or die("Chave incorreta!");        
+    $chavePessoalOK = false;
     while(!feof($myfile)) {
       $linha = fgets($myfile);
       $vetLinha = explode(";", $linha);
       if ($vetLinha[0] == $chavePessoal){
         echo "<h1>".$vetLinha[1].": seu amigo secreto é ".$vetLinha[2]."</h1>";
+        $chavePessoalOK = true;
       }
+    }
+    if (!$chavePessoalOK) {
+      echo "<h1> Chave pessoal incorreta </h1>";
     }
     fclose($myfile);
 ?>
