9fc443c282654de8f5bc5015f0761d3ea4848b87
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-24 16:58:24 -0300

    ok

--- gerar.php
+++ gerar.php
@@ -1,11 +1,11 @@
 <?php  
     $vetParticipante = $_POST['vetParticipante'];    
-    $vetAmigo = $_POST['vetParticipante'];    
+    $vetAmigo = $vetParticipante;    
     // $vetEmail = $_POST['vetEmail'];  
     $vetAmigo = [];
     $i = 0;
     if (is_array($vetAmigo) && is_array($vetParticipante)){
-        if (count($vetParticipante) > 0 && count($vetAmigo) > 0 && count($vetParticipante) == count($vetAmigo)){
+        if (count($vetParticipante) > 0 /*&& count($vetAmigo) > 0 && count($vetParticipante) == count($vetAmigo)*/){
             foreach($vetParticipante as $participante){        
                 $possivelAmigo = $vetParticipante[rand(0, count($vetParticipante)-1)];
                 while ($participante == $possivelAmigo || in_array($possivelAmigo, $vetAmigo)){            
@@ -18,7 +18,7 @@
             $chaveEvento = uniqid(true);
             echo "<h1> Evento Criado com sucesso <h1>";
             echo "<h2> Chave do evento:".$chaveEvento."<h2>";            
-            echo "<h3> Favor enviar as chaves pessoais para cada participante:</h3>";
+            echo "<h3> Informe aos participantes a chave do evento e suas chaves pessoais:</h3>";
             $myfile = fopen($chaveEvento.".txt", "w") or die("Unable to open file!");
             while ($i < count($vetParticipante)){                
                 $chave = uniqid(true);
@@ -30,7 +30,7 @@
             }
             fclose($myfile);           
         }  else {
-            echo "<h1> Não foi possível criar o evento de amigo secreto. Possíveis causas: <h1>";
+            echo "<h1> - Não foi possível criar o evento de amigo secreto. Possíveis causas: <h1>";
             echo "<h2> Nomes e Emails dos participantes são campos obrigatórios </h2>";    
         }
     } else {
