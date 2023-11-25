b5cb4134f5abec7f42ed641dcf38d496e775a7be
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-25 15:48:10 -0300

    ok

--- validade.php
+++ validade.php
@@ -22,7 +22,10 @@ class Validade
                     $data_expira_amigo_secreto = explode("-", $data_expira_amigo_secreto);
                     $data_atual = date("Y-m-d");
                     $data_atual = explode("-", $data_atual);
-                    // 2024, 2025 e etc. >= 2023+1 
+                    // 2024, 2025 e etc. >= 2023+1
+                    // echo var_dump($data_atual);
+                    // echo var_dump($data_expira_amigo_secreto); 
+                    // echo $data_atual[2].">=".$data_expira_amigo_secreto[2];
                     if ($data_atual[2] >= $data_expira_amigo_secreto[2]) {
                         // se for 2024 tenho que verificar o mes
                         if ($data_atual[2] - $data_expira_amigo_secreto[2] == 0) {
