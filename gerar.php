5a6a792ad2ad81890bd33f73ef2be9552b1f799d
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-25 14:46:59 -0300

    ok

--- gerar.php
+++ gerar.php
@@ -1,87 +1,103 @@
-<?php  
-    require_once "validade.php";
-    $URL = "https://amigosecretoigor.000webhostapp.com/";
-    if (!isset($_POST['vetParticipante'])) die("Acesso proibido!");
-    $vetParticipante = $_POST['vetParticipante'];
-    $i = 0;
-    if (is_array($vetParticipante)) {
-        while ($i < count($vetParticipante)){
-            if (empty($vetParticipante[$i])){
-                die("Não é permitido participante com nome em branco:".($i+1));
-            }
-            $i++;
-        }       
-    } else {
-        die("Cadastro incorreto de participantes");
-    }
+<?php
+define("URL", "https://amigosecretoigor.000webhostapp.com/");
 
-    if (is_array($vetParticipante)) {
-        for ($i = 0; $i < count($vetParticipante); $i++) {             
-            for ($j= $i+1; $j < count($vetParticipante); $j++) { 
-                // if (strcmp(trim(strtoupper($vetParticipante[$i])), trim(strtoupper($vetParticipante[$i])))){
-                if (trim(strtoupper($vetParticipante[$i])) == trim(strtoupper($vetParticipante[$j]))){
-                    die("Não é permitido participantes com nome exatamente iguais");
+class Geracao
+{
+    public function gera($vetParticipante)
+    {
+        // if (!isset($_POST['vetParticipante'])) die("Acesso proibido!");
+        $i = 0;
+        if (is_array($vetParticipante)) {
+            while ($i < count($vetParticipante)) {
+                if (empty($vetParticipante[$i])) {
+                    die("Não é permitido participante com nome em branco:" . ($i + 1));
                 }
+                $i++;
             }
-        }      
-    } else {
-        die("Cadastro incorreto de participantes");
-    }
+        } else {
+            die("Cadastro incorreto de participantes");
+        }
 
-
-    $vetAmigo = $vetParticipante;    
-    $vetAmigoPossivel = $vetParticipante;
-    // $vetEmail = $_POST['vetEmail'];  
-    $vetAmigo = [];
-    $i = 0;
-    if (is_array($vetAmigo) && is_array($vetParticipante)){
-        if (count($vetParticipante) > 0 /*&& count($vetAmigo) > 0 && count($vetParticipante) == count($vetAmigo)*/){
-            foreach($vetParticipante as $aux => $participante){   
-                // array_splice($vetAmigoPossivel, $aux, 1);  
-                shuffle($vetAmigoPossivel);  
-                $indicePossivelAmigo = 0;
-                $possivelAmigo = $vetAmigoPossivel[$indicePossivelAmigo];                              
-                while ($participante == $possivelAmigo || in_array(trim($possivelAmigo), $vetAmigo)){            
-                    if ($indicePossivelAmigo < count($vetAmigoPossivel)) {                        
-                        $possivelAmigo = $vetAmigoPossivel[$indicePossivelAmigo];
-                        $indicePossivelAmigo++;
-                    } else {                                                  
-                        $indicePossivelAmigo = 0;
-                        if ($aux == 0){
-                            $indicePossivelAmigo++;
-                        }
-                        $possivelAmigo = $vetAmigoPossivel[$indicePossivelAmigo];
-                        // die("Erro na geração do amigo secreto. Favor reiniciar o processo! <a href='".$URL."'> voltar </a>");
+        if (is_array($vetParticipante)) {
+            for ($i = 0; $i < count($vetParticipante); $i++) {
+                for ($j = $i + 1; $j < count($vetParticipante); $j++) {
+                    // if (strcmp(trim(strtoupper($vetParticipante[$i])), trim(strtoupper($vetParticipante[$i])))){
+                    if (trim(strtoupper($vetParticipante[$i])) == trim(strtoupper($vetParticipante[$j]))) {
+                        die("Não é permitido participantes com nome exatamente iguais");
                     }
                 }
-                $vetAmigo[$i] = $possivelAmigo;  
-                // array_splice($vetAmigoPossivel, $indicePossivelAmigo, 1);                     
-                // $vetAmigoPossivel[] = $vetParticipante[$i];
-                $i++;
             }
-            $i = 0;
-            $chaveEvento = uniqid(true);
-            echo "<h1> Evento Criado com sucesso <h1>";
-            echo "<h2> Chave do evento:".$chaveEvento."<h2>";            
-            echo "<h3> Informe aos participantes a chave do evento e suas chaves pessoais:</h3>";
-            $myfile = fopen($chaveEvento.".txt", "w") or die("Unable to open file!");
-            while ($i < count($vetParticipante)){                
-                $chave = uniqid(true);
-                echo  $vetParticipante[$i]." => <b>Chave Evento:</b>".$chaveEvento." <b>Chave Pessoal:</b>".$chave."<br><br>";
-                // participanteX tirou amigoY
-                $linha = $chave.";".$vetParticipante[$i].";".$vetAmigo[$i]."\n";
-                fwrite($myfile, $linha);
-                $i++;
-            }
-            echo "<h3> Todos os participantes devem acessar: <a href='".$URL."'> $URL </a> e VERIFICAR seu amigo (usando a chave do evento e sua chave pessoal) </h3>";
-            echo "<a href='$URL'> Voltar </a>";
-            fclose($myfile);           
-        }  else {
-            echo "<h1> - Não foi possível criar o evento de amigo secreto. Possíveis causas: <h1>";
-            echo "<h2> Nomes e Emails dos participantes são campos obrigatórios </h2>";    
+        } else {
+            die("Cadastro incorreto de participantes");
         }
-    } else {
-        echo "<h1> Não foi possível criar o evento de amigo secreto. Possíveis causas: <h1>";
-        echo "<h2> Nomes e Emails dos participantes são campos obrigatórios </h2>";
+
+
+        $vetAmigo = $vetParticipante;
+        $vetAmigoPossivel = $vetParticipante;
+        // $vetEmail = $_POST['vetEmail'];  
+        $vetAmigo = [];
+        $i = 0;
+        if (is_array($vetAmigo) && is_array($vetParticipante)) {
+            if (count($vetParticipante) > 0 /*&& count($vetAmigo) > 0 && count($vetParticipante) == count($vetAmigo)*/) {
+                foreach ($vetParticipante as $aux => $participante) {
+                    // array_splice($vetAmigoPossivel, $aux, 1);  
+                    shuffle($vetAmigoPossivel);
+                    $indicePossivelAmigo = 0;
+                    $possivelAmigo = $vetAmigoPossivel[$indicePossivelAmigo];
+                    while ($participante == $possivelAmigo || in_array(trim($possivelAmigo), $vetAmigo)) {
+                        if ($indicePossivelAmigo < count($vetAmigoPossivel)) {
+                            $possivelAmigo = $vetAmigoPossivel[$indicePossivelAmigo];
+                            $indicePossivelAmigo++;
+                        } else {
+                            $indicePossivelAmigo = 0;
+                            if ($aux == 0) {
+                                $indicePossivelAmigo++;
+                            }
+                            $possivelAmigo = $vetAmigoPossivel[$indicePossivelAmigo];
+                            // die("Erro na geração do amigo secreto. Favor reiniciar o processo! <a href='".$URL."'> voltar </a>");
+                        }
+                    }
+                    $vetAmigo[$i] = $possivelAmigo;
+                    // array_splice($vetAmigoPossivel, $indicePossivelAmigo, 1);                     
+                    // $vetAmigoPossivel[] = $vetParticipante[$i];
+                    $i++;
+                }
+                $i = 0;
+                $chaveEvento = uniqid(true);
+                echo "<h1> Evento Criado com sucesso <h1>";
+                echo "<h2> Chave do evento:" . $chaveEvento . "<h2>";
+                echo "<h3> Informe aos participantes a chave do evento e suas chaves pessoais:</h3>";
+                $myfile = fopen($chaveEvento . ".txt", "w") or die("Unable to open file!");
+                while ($i < count($vetParticipante)) {
+                    $chave = uniqid(true);
+                    echo  $vetParticipante[$i] . " => <b>Chave Evento:</b>" . $chaveEvento . " <b>Chave Pessoal:</b>" . $chave . "<br><br>";
+                    // participanteX tirou amigoY
+                    $linha = $chave . ";" . $vetParticipante[$i] . ";" . $vetAmigo[$i] . "\n";
+                    fwrite($myfile, $linha);
+                    $i++;
+                }
+                echo "<h3> Todos os participantes devem acessar: <a href='" . URL . "'> ".URL." </a> e VERIFICAR seu amigo (usando a chave do evento e sua chave pessoal) </h3>";
+                echo "<a href='".URL."'> Voltar </a>";
+                fclose($myfile);
+            } else {
+                echo "<h1> - Não foi possível criar o evento de amigo secreto. Possíveis causas: <h1>";
+                echo "<h2> Nomes e Emails dos participantes são campos obrigatórios </h2>";
+            }
+        } else {
+            echo "<h1> Não foi possível criar o evento de amigo secreto. Possíveis causas: <h1>";
+            echo "<h2> Nomes e Emails dos participantes são campos obrigatórios </h2>";
+        }
     }
+}
+
+require_once "validade.php";
+
+$validade = new Validade();
+$validade->valida();
+
+if (!isset($_POST['vetParticipante'])) die("Acesso proibido!");
+$vetParticipante = $_POST['vetParticipante'];
+$geracao = new Geracao();
+$geracao->gera($vetParticipante);
+
 ?>
--- validade.php
+++ validade.php
@@ -1,35 +1,52 @@
 <?php
 
-$codigo = ["gerar.php", "index.html", "validade.php", "verificar.php", ".git", "README.md"];
+class Validade
+{
+    // private $ok;
 
-if ($handle = opendir('.')) {
-    while (false !== ($entry = readdir($handle))) {
-        if ($entry != "." && $entry != ".." && !in_array($entry, $codigo)) {
-            echo "$entry <br>";
-            
-            $futureDate = date('Y-m-d', strtotime('+1 year', filectime($entry)));
-            $futureDate = explode("-", $futureDate);
-            $data_atual = date("Y-m-d");
-            $data_atual = explode("-", $data_atual);
-            // 2024, 2025 e etc. >= 2023+1 
-            if ($futureDate[2] >= $data_atual[2]+1){
-                // se for 2024 tenho que verificar o mes
-                if ($futureDate[2] - $data_atual[2]+1 == 0) {
-                    // se o mes eh maior => deleta
-                    if($futureDate[1] > $data_atual[1]){
-                        unlink($entry);
-                    }  
-                    // se for o mesmo mes mas o dia eh maior => deleta
-                    else if ($futureDate[1] == $data_atual[1] && $futureDate[0] > $data_atual[0]){
-                        unlink($entry);
+    public function __construct()
+    {
+        // $this->ok = true;
+    }
+
+    public function valida()
+    {
+
+
+
+        $codigo = ["gerar.php", "index.html", "validade.php", "verificar.php", ".git", "README.md"];
+
+        if ($handle = opendir('.')) {
+            while (false !== ($entry = readdir($handle))) {
+                if ($entry != "." && $entry != ".." && !in_array($entry, $codigo)) {
+                    echo "$entry <br>";
+
+                    $futureDate = date('Y-m-d', strtotime('+1 year', filectime($entry)));
+                    $futureDate = explode("-", $futureDate);
+                    $data_atual = date("Y-m-d");
+                    $data_atual = explode("-", $data_atual);
+                    // 2024, 2025 e etc. >= 2023+1 
+                    if ($futureDate[2] >= $data_atual[2] + 1) {
+                        // se for 2024 tenho que verificar o mes
+                        if ($futureDate[2] - $data_atual[2] + 1 == 0) {
+                            // se o mes eh maior => deleta
+                            if ($futureDate[1] > $data_atual[1]) {
+                                unlink($entry);
+                            }
+                            // se for o mesmo mes mas o dia eh maior => deleta
+                            else if ($futureDate[1] == $data_atual[1] && $futureDate[0] > $data_atual[0]) {
+                                unlink($entry);
+                            }
+                        } else {
+                            // se a diferença for maior => deleta
+                            unlink($entry);
+                        }
                     }
-                } else {
-                    // se a diferença for maior => deleta
-                    unlink($entry);
                 }
             }
+            closedir($handle);
         }
     }
-    closedir($handle);
 }
+
 ?>
